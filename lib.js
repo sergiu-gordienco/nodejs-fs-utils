dbb1b79962d8b156d41a1edc7c074eb2515f6dce
Sergiu Gordienco Vasile <sergiu.gordienco@gmail.com>
2019-12-16 02:44:50 +0200

    minor fixes

--- lib.js
+++ lib.js
@@ -1,5 +1,3 @@
-var mrequire	= require("module-require");
-
 var nodejsFsUtils	= {
 	ensureFile	: false,
 	ensureFileSync	: false,
@@ -15,52 +13,44 @@ var nodejsFsUtils	= {
 var getLibrary	= function (list) {
 	var lib	= {};
 	if (list.indexOf('copy') !== -1) {
-		mrequire("copy", __dirname + "/fs/copy/index.js");
-		lib.copy	= mrequire("copy");
-		lib.copySync	= mrequire("copy").sync;
+		lib.copy	= require("./fs/copy");
+		lib.copySync	= require("./fs/copy").sync;
 	}
 	if (list.indexOf('rmdirs') !== -1) {
-		mrequire("rmdirs", __dirname + "/fs/rmdirs/index.js");
-		lib.rmdirs	= mrequire("rmdirs");
-		lib.rmdirsSync	= mrequire("rmdirs").sync;
-		lib.remove	= mrequire("rmdirs");
-		lib.removeSync	= mrequire("rmdirs").sync;
+		lib.rmdirs	= require("./fs/rmdirs");
+		lib.rmdirsSync	= require("./fs/rmdirs").sync;
+		lib.remove	= require("./fs/rmdirs");
+		lib.removeSync	= require("./fs/rmdirs").sync;
 	}
 	if (list.indexOf('fsize') !== -1) {
-		mrequire("fsize", __dirname + "/fs/fsize/index.js");
-		lib.fsize	= mrequire("fsize");
-		lib.fsizeSync	= mrequire("fsize").sync;
+		lib.fsize	= require("./fs/fsize");
+		lib.fsizeSync	= require("./fs/fsize").sync;
 	}
 	if (list.indexOf('walk') !== -1) {
-		mrequire("walk", __dirname + "/fs/walk/index.js");
-		lib.walk	= mrequire("walk");
-		lib.walkSync	= mrequire("walk").sync;
+		lib.walk	= require("./fs/walk");
+		lib.walkSync	= require("./fs/walk").sync;
 	}
 	if (list.indexOf('mkdirs') !== -1) {
-		mrequire("mkdirs", __dirname + "/fs/mkdirs/index.js");
-		lib.mkdirs	= mrequire("mkdirs");
-		lib.mkdirsSync	= mrequire("mkdirs").sync;
-		lib.ensureDir	= mrequire("mkdirs");
-		lib.ensureDirSync	= mrequire("mkdirs").sync;
+		lib.mkdirs	= require("./fs/mkdirs");
+		lib.mkdirsSync	= require("./fs/mkdirs").sync;
+		lib.ensureDir	= require("./fs/mkdirs");
+		lib.ensureDirSync	= require("./fs/mkdirs").sync;
 	}
 	if (list.indexOf('move') !== -1) {
-		mrequire("move", __dirname + "/fs/move/index.js");
-		lib.move	= mrequire("move");
-		lib.moveSync	= mrequire("move").sync;
+		lib.move	= require("./fs/move");
+		lib.moveSync	= require("./fs/move").sync;
 	}
 	if (list.indexOf('emptydir') !== -1) {
-		mrequire("emptydir", __dirname + "/fs/emptydir/index.js");
-		lib.emptyDir	= mrequire("emptydir");
-		lib.emptyDirSync	= mrequire("emptydir").sync;
-		lib.isEmpty	= mrequire("emptydir").isEmpty;
-		lib.isEmptySync	= mrequire("emptydir").isEmpty.sync;
+		lib.emptyDir	= require("./fs/emptydir");
+		lib.emptyDirSync	= require("./fs/emptydir").sync;
+		lib.isEmpty	= require("./fs/emptydir").isEmpty;
+		lib.isEmptySync	= require("./fs/emptydir").isEmpty.sync;
 	}
 	if (list.indexOf('output') !== -1) {
-		mrequire("output", __dirname + "/fs/output/index.js");
-		lib.createFile	= mrequire("output").createFile;
-		lib.createFileSync	= mrequire("output").createFile.sync;
-		lib.ensureFile	= mrequire("output").createFile;
-		lib.ensureFileSync	= mrequire("output").createFile.sync;
+		lib.createFile	= require("./fs/output").createFile;
+		lib.createFileSync	= require("./fs/output").createFile.sync;
+		lib.ensureFile	= require("./fs/output").createFile;
+		lib.ensureFileSync	= require("./fs/output").createFile.sync;
 	}
 	return lib;
 };
